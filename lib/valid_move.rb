b1517072209057de00c988d39e406b3f8e171652
Brian Ringley <brian.ringley@wework.com>
2018-02-16 16:28:17 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,8 @@ end
 def position_taken?(board, index)
   if (board[index] == "") || (board[index] == " ") || (board[index] == nil)
     false
-  else (board[index] == "X") || (board[index] == "O")
+  else 
+    #(board[index] == "X") || (board[index] == "O")
     true
   end
 end
